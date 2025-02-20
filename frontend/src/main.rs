9da15a154f2b98be278e2d34f1c47125114c6858
diogogomesaraujo <118282799+diogogomesaraujo@users.noreply.github.com>
2025-02-20 01:27:18 +0000

    adding and removing

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -40,7 +40,7 @@ pub fn Padding() -> Element {
 #[component]
 pub fn Hero() -> Element {
     let mut tasks = use_signal(|| Vec::<Task>::new());
-    let mut input_text = use_signal(|| String::new());
+    let mut input_text = use_signal(|| String::from("Type here..."));
 
     let get_tasks = move || async move {
         let response = reqwest::get("http://localhost:3000/tasks")
@@ -51,8 +51,6 @@ pub fn Hero() -> Element {
             .unwrap();
 
         tasks.set(response);
-
-        println!("{:?}", tasks);
     };
 
     let add_task = move |text: String| async move {
@@ -62,7 +60,20 @@ pub fn Hero() -> Element {
         client
             .post("http://localhost:3000/tasks")
             .header("Content-Type", "application/json")
-            .body(text)
+            .body(format!("\"{}\"", text))
+            .send()
+            .await
+            .unwrap();
+    };
+
+    let remove_task = move |text: String| async move {
+        let client = reqwest::Client::new();
+        let id: u32 = text.trim().parse().unwrap();
+
+        // handle response
+        client
+            .delete(format!("http://localhost:3000/tasks/{}", id))
+            .header("Content-Type", "application/json")
             .send()
             .await
             .unwrap();
@@ -74,26 +85,30 @@ pub fn Hero() -> Element {
         div {
             id: "hero",
             div {
+                id: "buttons",
                 input {
-                    // and what to do when the value changes
+                    id: "input",
                     value: "{input_text}",
                     oninput: move |event| input_text.set(event.value())
                 }
             }
+            Padding {}
             div {
                 {
                     rsx!(
                         {tasks.iter().map(|item| rsx! {
                             div { id: "buttons",
-                                a { id: "task", "{item.content}" }
+                                a { id: "task", strong { "{item.id}." }, "   {item.content}" }
                             }
                         })}
                     )
                 }
             },
             Padding {}
+            Padding {}
             div {
-                id: "buttons", button {
+                id: "buttons",
+                button {
                     onclick: move |_| {
                         let task_text = input_text.clone();
                         spawn(async move {
@@ -101,7 +116,19 @@ pub fn Hero() -> Element {
                             add_task(task_text.to_string()).await;
                         });
                     },
-                    id: "add_task", "Add a Task", }
+                    id: "add_task", "Add a Task",
+                },
+                Padding {}
+                button {
+                    onclick: move |_| {
+                        let task_text = input_text.clone();
+                        spawn(async move {
+                            println!("Added Task: {}", task_text);
+                            remove_task(task_text.to_string()).await;
+                        });
+                    },
+                    id: "remove_task", "Remove a Task",
+                }
             }
         }
     }
